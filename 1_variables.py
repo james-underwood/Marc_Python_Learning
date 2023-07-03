89b69e9dbcc3b26dbf04a32234b659d1bb924473
James <61007893+james-underwood@users.noreply.github.com>
2023-07-03 15:46:17 -0400

    Create 1_variables.py

---
+++ 1_variables.py
@@ -0,0 +1,17 @@
+# Introduction to Variables
+# Variables are used to store data in Python. They can hold different types of values, such as numbers or text.
+
+# Example usage:
+name = "John"
+age = 25
+height = 1.75
+
+print("My name is", name)
+print("I am", age, "years old")
+print("My height is", height, "meters")
+
+# Challenge: Create variables to store the following data:
+# - Your favorite color
+# - Your birth year
+# - Your shoe size
+# Print out the values of these variables.
