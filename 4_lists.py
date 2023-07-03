2fe65a4cd19a899842ec4ea3880ae8dd8805aa07
James <61007893+james-underwood@users.noreply.github.com>
2023-07-03 15:47:08 -0400

    Create 4_lists.py

---
+++ 4_lists.py
@@ -0,0 +1,15 @@
+# Introduction to Lists
+# Lists are used to store multiple items in a single variable. They can hold different data types and can be modified.
+
+# Example usage:
+fruits = ["apple", "banana", "orange"]
+
+print(fruits[0])  # Accessing the first element
+print(len(fruits))  # Getting the length of the list
+
+fruits.append("grape")  # Adding a new element to the end of the list
+fruits.remove("banana")  # Removing an element from the list
+
+print(fruits)  # Printing the modified list
+
+# Challenge: Create a list of your favorite movies and print each movie name on a separate line.
