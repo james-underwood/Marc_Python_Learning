c2fe1b38c36adea6fbc58ce701120405780355eb
James <61007893+james-underwood@users.noreply.github.com>
2023-07-03 15:47:35 -0400

    Create 5_functions.py

---
+++ 5_functions.py
@@ -0,0 +1,11 @@
+# Introduction to Functions
+# Functions allow us to group a set of instructions together and reuse them by calling the function.
+
+# Example usage:
+def greet(name):
+    print("Hello, " + name + "!")
+
+greet("Alice")  # Calling the function with a specific name
+greet("Bob")
+
+# Challenge: Write a function that takes two numbers as parameters and returns their sum.
