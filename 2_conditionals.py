b70b92d850a4b7623ad2a88cafb777959b729bea
James <61007893+james-underwood@users.noreply.github.com>
2023-07-03 15:46:33 -0400

    Create 2_conditionals.py

---
+++ 2_conditionals.py
@@ -0,0 +1,14 @@
+# Introduction to Conditionals
+# Conditionals allow us to make decisions in our code based on certain conditions being true or false.
+
+# Example usage:
+age = 17
+
+if age >= 18:
+    print("You are eligible to vote.")
+else:
+    print("You are not eligible to vote.")
+
+# Challenge: Write a program that takes a user's input for their age and prints:
+# - "You can watch a PG-13 movie" if they are 13 years or older
+# - "You can watch a G-rated movie" otherwise
