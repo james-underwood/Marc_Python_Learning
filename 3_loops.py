bb33d4cda403076f1558cd596364178a49c74db6
James <61007893+james-underwood@users.noreply.github.com>
2023-07-03 15:46:48 -0400

    Create 3_loops.py

---
+++ 3_loops.py
@@ -0,0 +1,8 @@
+# Introduction to Loops
+# Loops allow us to repeat a set of instructions multiple times until a certain condition is met.
+
+# Example usage:
+for i in range(5):
+    print("Hello, world!")
+
+# Challenge: Write a program that prints the numbers from 1 to 10 using a while loop.
